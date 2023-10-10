5354f2c24518564cc89f24107077da2a703fe658
mywrina331 <143722088+mywrina331@users.noreply.github.com>
2023-10-10 15:07:43 +0300

    Add files via upload

---
+++ models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+class Advertisement(models.Model):
+    title = models.CharField('Заголовок', max_length=128)
+    description = models.TextField('Описание')
+    price = models.DecimalField('Цена', max_length=10, decimal_places=2, max_digits=20)
+    auction = models.BooleanField('Торг', help_text='Отметьте, если торг уместен')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta():
+        db_table = 'advertisements'
+
+    def __str__(self):
+        return f'<Advertisement: Advertisement(id={self.id}, title={self.title}, price={self.price})>'
